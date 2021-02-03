1616c04505043a4cf8d74e620fefea7228df498c
Lorna Espinosa Cuello <lornatoon@hotmail.com>
2021-02-03 18:24:00 -0400

    Update test.py
    test open and read file

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
 import unittest
 import main
+import os
 
+TESTDATA_FILENAME = os.path.join(os.path.dirname(__file__), 'testdata.txt')
 
 class Test(unittest.TestCase):
-    pass
+    def test_readFile(self):
+        self.testfile = open(TESTDATA_FILENAME)
+        self.testdata = self.testfile.read()
 
 if __name__ == '__main__':
     unittest.main()

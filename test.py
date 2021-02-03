8ad2792f322fb19092d1fb5fef35f18332ab3a1f
Lorna Espinosa Cuello <lornatoon@hotmail.com>
2021-02-03 18:08:13 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,9 @@
 import unittest
+import main
 
 
 class Test(unittest.TestCase):
-    def test_bubble_sort_clear(self):
-        self.assertEqual(True, True)
-
+    pass
 
 if __name__ == '__main__':
     unittest.main()

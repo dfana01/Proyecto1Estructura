092b4ca659b2dc74c61f804248327f83290f06f1
Dante Faña Badia <dfana@dfb.com.do>
2021-02-03 00:55:48 -0400

    Add basic structure

---
+++ test.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class Test(unittest.TestCase):
+    def test_bubble_sort_clear(self):
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+    unittest.main()

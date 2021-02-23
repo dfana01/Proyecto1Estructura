c3d5bf6b2a9a4deb37b645cd95c748f1c0aee162
Dante Faña Badia <dfana@dfb.com.do>
2021-02-23 01:30:43 -0400

    Add integration tests, equalize w test, complexity and enhance docs

--- main.py
+++ main.py
@@ -1,26 +1,58 @@
+import re
 from os import scandir
+import sys
 
 # 76/100
 
 # no se hicieron tests de integracion que validen el algoritmo funcionando en conjunto -4
 # no se contempla cambio de variables -20
 
+
 def read_file(path):
+    """
+    :complexity
+        - time: O(1), Dado que solo se ejecuta una instrucción.
+        - space: O(1), Dado que solo se ejecuta una instrucción de lectura y se almacenara en un variable.
+    """
     file = open(path, 'r')
     text = file.read()
     file.close()
     return text
 
 
+def equalize_functions_n_vars(s):
+    """
+    :complexity
+        - time: O(n), dado que se recorre 2 veces el string pero estos no estan anidados.
+        - space: O(1), pues no se usan nuevas variables.
+    """
+    return re.sub(r"\w+\s+[=]\s+", "var1=", re.sub(r"\bdef\s+\w+\b\(", "func1(", s))
+
+
 def clean(s):
+    """
+    :complexity
+        - time: O(n), dado que se recorre 3 veces el string pero estos no estan anidados.
+        - space: O(1), pues no se usan nuevas variables.
+    """
     return s.replace(" ", "").replace("\t", "").replace("\n", "")
 
 
 def calculate_similarity(s1, s2, difference):
+    """
+    :complexity
+        - time: O(1), solo se ejecuta una instrucción
+        - space: O(1), solo se ejecuta una instrucción sin craer nuevas variables.
+    """
     return round((1 - (difference / max(len(s1), len(s2)))) * 100, 2)
 
 
 def levenshtein_distance(s1, s2):
+    """
+    :complexity
+        - time: O(NxM), dado que se comparan todos los elementos dentro de la matrix.
+        - space: O(NxM), porque se genera una matrix para almacenar todas las comparaciones.
+    """
     m, n = len(s1) + 1, len(s2) + 1
     d = [[0 for _ in range(n)] for _ in range(m)]
 
@@ -39,34 +71,71 @@ def levenshtein_distance(s1, s2):
 
 
 def equal(s1, s2):
+    """
+    :complexity
+        - time: O(1), Pues solo se comparan dos string.
+        - space:  O(1), Pues no hay nuevas varibles.
+    """
     return s1 == s2
 
 
 def read_folder_meta(path):
+    """
+    :complexity
+        - time: O(n), Pues se recorren todos los archivos para ver cuales aplican para ser escaneados y su mayor
+        complejidad puede ser n donde n es la cantidad máxima de archivo.
+        - space: O(1), Dado que el mismo genera una lista donde la mayor complejidad son todos los archivos dentro de
+        la carpeta enviada por parámetro pero no la almacena en ninguna variable.
+    """
     return filter(lambda x: x.name.endswith('.java') or x.name.endswith('.c') or x.name.endswith('.py'), scandir(path))
 
 
-if __name__ == '__main__':
-    base_path = input("Enter a folder:")  # pueden usar sys.args
-    print(f"==========================Folder: {base_path}==========================")
+def plagiarism_checker(base_path):
+    """
+    :complexity
+        - time: Pues recorre todos los archivos dentro de folder y se vuelven a recorrer los otra vez para conparar
+        cada uno de ellos entre si. A pesar de que llamos las funcion de comparacion, igualdad y limpieza todas estas
+        funciones tiene una complejidad menor a O(n^2) por lo tanto se toma la complejidad mayor.
+        - space: O(1), Dado que se utiliza una sola variable para los resultados y no se usa ninguna lista su
+        complejidad es constante.
+    :enhance: se puede intentar bajar la complejidad de tiempo algoritmo calculando la comparaciones y almacenando
+    asi sacrificando la complejidad de espacio a O(n) y tratar de bajar la complejidad de tiempo no calculando nueva vez
+    los datos calculados.
+    """
+    results = f"==========================Folder: {base_path}==========================\n"
     for entry0 in read_folder_meta(base_path):
         content0 = read_file(entry0.path)
-        print(f"- {entry0.name}")
+        results += f"- {entry0.name}\n"
         for entry1 in read_folder_meta(base_path):
             if entry1.name == entry0.name:
                 continue
             content1 = read_file(entry1.path)
 
-            diff_with_tap_n_space, diff = levenshtein_distance(content0, content1), \
-                                          levenshtein_distance(clean(content0), clean(content1))
+            diff_with_tap_n_space = levenshtein_distance(content0, content1)
+            diff_with_equalize_function = levenshtein_distance(equalize_functions_n_vars(clean(content0)),
+                                                               equalize_functions_n_vars(clean(content1)))
+            diff = levenshtein_distance(clean(content0), clean(content1))
 
-            probability_with_tap_n_space, probability = calculate_similarity(content0, content1, diff_with_tap_n_space), \
-                                                        calculate_similarity(clean(content0), clean(content1), diff)
+            probability_with_tap_n_space = calculate_similarity(content0, content1, diff_with_tap_n_space)
+            probability_with_equalize_function = calculate_similarity(equalize_functions_n_vars(clean(content0)),
+                                                                      equalize_functions_n_vars(clean(content1)),
+                                                                      diff_with_equalize_function)
+            probability = calculate_similarity(clean(content0), clean(content1), diff)
 
-            print(f"\t|_____{entry1.name}")
-            print(f"\t\t|_____there are {probability}% of plagiarism without taps and spaces")
-            print(f"\t\t|_____there are {probability_with_tap_n_space}% of plagiarism with taps and spaces")
+            results += f"\t|_____{entry1.name}\n"
+            results += f"\t\t\t|_____there are {probability}% of plagiarism without taps and spaces\n"
+            results += f"\t\t\t|_____there are {probability_with_tap_n_space}% of plagiarism with taps and spaces\n"
+            results += f"\t\t\t|_____there are {probability_with_equalize_function}% of plagiarism with equalize " \
+                       f"functions and variables\n "
             if equal(clean(content0), clean(content1)):
-                print(f"\t\t|_____files are equal")
-            if probability > 70 and probability_with_tap_n_space > 30:
-                print(f"\t\t|_____is possible that the user try to hide plagiarism with taps and spaces")
+                results += f"\t\t\t|_____files are equal\n"
+            if probability_with_tap_n_space <= 80 and probability >= 50:
+                results += f"\t\t\t|_____is possible that the user try to hide plagiarism with taps and spaces\n"
+            if probability_with_tap_n_space <= 80 and probability_with_equalize_function >= 50:
+                results += f"\t\t\t|_____is possible that the user try to hide plagiarism renaming function and/or " \
+                           f"variables\n"
+    return results
+
+
+if __name__ == '__main__':
+    print(plagiarism_checker(sys.argv[1])) # pueden usar sys.args
